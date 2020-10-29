3e5ddbe06f28303c586afecb7ac960272a4b37df
ozzaynetworks <ozzaynetworks@gmail.com>
2020-10-29 22:14:52 +0300

    initial files

---
+++ assets/js/main.js
@@ -0,0 +1,113 @@
+$(document).ready(function(){
+    $('.flatpickr-input').flatpickr({
+
+        altInput: true,
+        altFormat: "F j",
+        dateFormat: "Y-m-d",
+    });
+
+    checkForImage();
+
+    function checkForImage(){
+        $('.form-body .control-buttons img').each(function(){
+      
+            var the_img=$(this).attr('src');
+         //    alert(the_img);
+            if(the_img!=="#"){
+                $(this).parent().addClass('no-img-bg');
+     
+            }
+            if(the_img==="#"){
+             $(this).parent().removeClass('no-img-bg');
+         }
+     
+         });
+    }
+
+    $('.control-buttons span').on('click', function(){
+        $(this).addClass('selected');
+        $(this).parent().siblings().children('span').removeClass('selected');
+        var newUrl=$(this).children('img').attr('src');
+        if(newUrl!=="#"){
+            $('.upload-img-cont img').attr('src',newUrl);
+            $('.upload-img-cont h6').addClass('d-none');
+            $('.activation-form-container .upload-img-cont').addClass('no-img-bg');
+
+        }
+        else{
+            $('.upload-img-cont img').attr('src','#');
+            $('.upload-img-cont h6').removeClass('d-none');
+            $('.activation-form-container .upload-img-cont').removeClass('no-img-bg');
+
+        }
+
+    });
+
+    $('.logo-input').on('change', function(){
+        var theId=$(this).attr("id");
+        var theInput;
+        var lastCharacter=theId[theId.length-1];
+        theInput="input"+lastCharacter;
+
+        file_changed(theId,theInput);
+    });
+
+    function file_changed(theId,theInput){
+      
+        var selectedFile = document.getElementById(theId).files[0];
+        var img = document.getElementById('selected-image');
+        var img2 = document.getElementById(theInput); 
+        $('.upload-img-cont h6').addClass('d-none');
+        $('#'+theInput).parent().addClass('no-img-bg');
+
+       
+
+        $('.activation-form-container .upload-img-cont').addClass('no-img-bg');
+       
+        // var img = document.getElementsByClassName(theId);
+      
+        var reader = new FileReader();
+        reader.onload = function(){
+           img.src = this.result;
+           img2.src=this.result;
+          
+        }
+
+      
+        reader.readAsDataURL(selectedFile);
+        
+       }
+
+       $('.form-links .nav li').on('click', function(){
+           $(this).children('a').addClass('active');
+           $(this).siblings().children('a').removeClass('active');
+           var theIndex=$(this).index();
+           var theIndex=theIndex+1;
+           var theClass=".slider-container"+theIndex;
+           $(theClass).removeClass('d-none').siblings().addClass('d-none');
+           
+       });
+
+       $('.form-btns .btn-next').on('click', function(){
+           var theParent=$(this).parent().parent().parent().parent().parent()
+            theParent.addClass("d-none");
+            theParent.next().removeClass("d-none");
+            var theParentIndex=theParent.index();
+            $('.form-links .nav li a').removeClass('active');
+            $('.form-links .nav li').eq(theParentIndex+1).children('a').addClass('active');
+           
+       });
+
+       $('.form-btns .btn-prev').on('click', function(){
+        var theParent=$(this).parent().parent().parent().parent().parent()
+         theParent.addClass("d-none");
+         theParent.prev().removeClass("d-none");
+         var theParentIndex=theParent.index();
+         $('.form-links .nav li a').removeClass('active');
+         $('.form-links .nav li').eq(theParentIndex-1).children('a').addClass('active');
+        
+    });
+
+
+
+});
